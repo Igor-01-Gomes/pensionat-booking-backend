cdab4cf53cc71453ed5018f50f7849d13a1c509c
niklasdahlen <dahlstromniklas1@gmail.com>
2026-05-13 10:40:08 +0200

    feature/roomService

---
+++ src/main/java/com/pensionat/room/service/RoomService.java
@@ -0,0 +1,26 @@
+package com.pensionat.room.service;
+
+import com.pensionat.room.model.RoomEntity;
+import com.pensionat.room.repository.RoomRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class RoomService {
+
+    private final RoomRepository roomRepository;
+
+    public RoomService(RoomRepository roomRepository) {
+        this.roomRepository = roomRepository;
+    }
+
+    public List<RoomEntity> getAllRooms(){
+        return roomRepository.findAll();
+    }
+
+    public Optional<RoomEntity> getRoomById(int roomId){
+        return roomRepository.findById(roomId);
+    }
+}
