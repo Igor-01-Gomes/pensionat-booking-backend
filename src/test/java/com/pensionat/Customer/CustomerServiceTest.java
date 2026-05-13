0b0c699e7db7ea5641ad51888fe81b9aead23550
Daniel Lyytikäinen <daniel.lyytikainen@yh.nackademin.se>
2026-05-13 14:46:31 +0200

    test(customer): add unit test for exception handling on createCustomerfailure

--- src/test/java/com/pensionat/Customer/CustomerServiceTest.java
+++ src/test/java/com/pensionat/Customer/CustomerServiceTest.java
@@ -10,8 +10,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -53,6 +52,7 @@ import static org.mockito.Mockito.when;
         assertEquals("Lyytikäinen", testResult.getLastName());
         assertEquals("Test@test.com", testResult.getEmail());
         assertEquals("+46701234567", testResult.getPhoneNumber());
+        assertNotEquals("Patric", testResult.getFirstName());
 
         verify(customerRepository).save(any(CustomerEntity.class));
 
@@ -67,5 +67,16 @@ import static org.mockito.Mockito.when;
         when(customerRepository.save(any(CustomerEntity.class))).thenReturn(new CustomerEntity());
         customerService.createCustomer(request);
         verify(customerRepository).save(any(CustomerEntity.class));
+    }
+    @Test
+    void givenRepositoryFails_WhenCreateCustomer_ThenThrowException() {
+        CreateCustomerRequest request = new CreateCustomerRequest("Daniel",
+                "Lyytikäinen",
+                "Test@test.com",
+                "Password123",
+                "+46701234567");
+        when(customerRepository.save(any(CustomerEntity.class))).thenThrow(new RuntimeException("Database error!"));
+
+        assertThrows(RuntimeException.class,() -> customerService.createCustomer(request));
     }
 }
