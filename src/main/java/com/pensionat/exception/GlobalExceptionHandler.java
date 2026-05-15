e7981722e0f2e692f3e2b3c13bfabcc8bf42f7e6
Daniel Lyytikäinen <daniel.lyytikainen@yh.nackademin.se>
2026-05-15 12:24:08 +0200

    Changed GlobalExceptionHandler

--- src/main/java/com/pensionat/exception/GlobalExceptionHandler.java
+++ src/main/java/com/pensionat/exception/GlobalExceptionHandler.java
@@ -1,7 +1,21 @@
 package com.pensionat.exception;
 
-public class GlobalExceptionHandler extends RuntimeException {
-    public GlobalExceptionHandler(String message) {
-        super(message);
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+
+    @ExceptionHandler(NotFoundException.class)
+    ResponseEntity<String> handleNotFoundException(NotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(BadRequestException.class)
+    ResponseEntity<String> handleBadRequestException(BadRequestException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
     }
 }
