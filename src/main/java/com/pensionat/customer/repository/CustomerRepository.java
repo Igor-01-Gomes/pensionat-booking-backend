dfdcc5a1f21d0e647c88215ba4f7831b511dc3a8
Daniel Lyytikäinen <daniel.lyytikainen@yh.nackademin.se>
2026-05-20 15:18:08 +0200

    added findByEmail

--- src/main/java/com/pensionat/customer/repository/CustomerRepository.java
+++ src/main/java/com/pensionat/customer/repository/CustomerRepository.java
@@ -3,7 +3,10 @@ package com.pensionat.customer.repository;
 import com.pensionat.customer.model.CustomerEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CustomerRepository
         extends JpaRepository<CustomerEntity, Long> {
     boolean existsByEmailAndIdNot(String email, Long id);
+    Optional<CustomerEntity> findByEmail(String email);
 }
